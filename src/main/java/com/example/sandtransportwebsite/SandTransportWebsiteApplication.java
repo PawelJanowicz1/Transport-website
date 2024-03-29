ef9e2ad15a90ae35593eafb1bd6c0bad6e60d847
Paweł Janowicz <janowiczpawel4@gmail.com>
2024-03-29 23:18:43 +0100

    Initial commit

---
+++ src/main/java/com/example/sandtransportwebsite/SandTransportWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.sandtransportwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandTransportWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandTransportWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sandtransportwebsite/SandTransportWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sandtransportwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandTransportWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
