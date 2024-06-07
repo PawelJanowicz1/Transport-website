62ed0da4ec8386e2a012e461ccda4c7970fd8cbd
Paweł Janowicz <janowiczpawel4@gmail.com>
2024-06-07 19:32:52 +0200

    adding crud for client

--- src/main/java/com/example/sandtransportwebsite/model/Client.java
+++ src/main/java/com/example/sandtransportwebsite/model/Client.java
@@ -1,11 +1,13 @@
 package com.example.sandtransportwebsite.model;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Data
 @Table(name = "clients")
 public class Client {
     @Id
