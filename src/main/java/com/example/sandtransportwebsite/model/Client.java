e6e5f2f28d3ad67028e85d73b9ce82247090d717
Paweł Janowicz <janowiczpawel4@gmail.com>
2024-06-20 21:34:57 +0200

    adding static images

--- src/main/java/com/example/sandtransportwebsite/model/Client.java
+++ src/main/java/com/example/sandtransportwebsite/model/Client.java
@@ -3,7 +3,6 @@ package com.example.sandtransportwebsite.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
--- src/main/java/com/example/sandtransportwebsite/repository/ClientRepository.java
+++ src/main/java/com/example/sandtransportwebsite/repository/ClientRepository.java
@@ -5,5 +5,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
-    Client getClientById(Long id);
 }
