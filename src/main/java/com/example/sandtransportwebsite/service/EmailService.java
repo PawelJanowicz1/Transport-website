5e0c979ab35e1f1a568ebe94499d742ff34750fe
Paweł Janowicz <janowiczpawel4@gmail.com>
2024-07-02 21:41:42 +0200

    adding new things

--- src/main/java/com/example/sandtransportwebsite/service/EmailService.java
+++ src/main/java/com/example/sandtransportwebsite/service/EmailService.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
 
